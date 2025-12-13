2a836ca5db9971004b69a20b4a4b01281eabee93
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-13 19:40:03 +0200

    feat(ui): add Tailwind CSS via @tailwindcss/vite and apply basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>RepoRadar</div>
+      <h1 className="text-3xl font-bold underline text-red-500">RepoRadar!</h1>
     </>
   );
 }
